ab0e897d12b6d8e6ff8b4df16521d7618b2712ec
ymmn <abdnimery92@gmail.com>
2013-11-14 23:10:21 -0600

    drag courses in

--- src/static/js/controllers/mainCtrl.js
+++ src/static/js/controllers/mainCtrl.js
@@ -20,5 +20,59 @@ angular.module('schedulePlanner').controller('MainCtrl',
           duration: 50,
         }
       ];
+
+      $scope.hours = [
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16
+      ];
+
+      var course_to_coursetime = {
+        "ANTH 100" : [
+          {
+            "time_start": 480,
+            "day": 1,
+            "time_end": 600
+          },
+          {
+            "time_start": 600,
+            "day": 2,
+            "time_end": 660
+          }
+        ]
+      };
+
+      $scope.days = [
+        {
+          name: 'Mon',
+          acronym: '1'
+        },
+        {
+          name: 'Tues',
+          acronym: '2'
+        },
+        {
+          name: 'Wed',
+          acronym: '3'
+        },
+        {
+          name: 'Thurs',
+          acronym: '4'
+        },
+        {
+          name: 'Fri',
+          acronym: '5'
+        }
+      ];
+
+      window.scope = $scope;
+      
     }
 );
