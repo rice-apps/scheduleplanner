e67a5bf41df7dfbfb3b699702b7c70ed51dc1425
Matthew Schurr <mschurr@rice.edu>
2015-03-28 16:18:16 -0500

    Analytics.

--- backend/views/SchedulePlanner.blade.php
+++ backend/views/SchedulePlanner.blade.php
@@ -4,3 +4,13 @@
 @style(URL::asset('frontend/org/riceapps/stylesheets/view.css'))
 
 <script src="{{{ URL::asset('frontend/prod.js') }}}" type="text/javascript"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-61282211-1', 'auto');
+  ga('send', 'pageview');
+</script>
