0e067bab152c390503e0b3fc263b3c08fcfc7719
Matthew Schurr <mschurr@rice.edu>
2015-03-28 20:44:22 -0500

    Updated directions.

--- backend/views/SchedulePlanner.blade.php
+++ backend/views/SchedulePlanner.blade.php
@@ -5,6 +5,7 @@
 
 <script src="{{{ URL::asset('frontend/prod.js') }}}" type="text/javascript"></script>
 
+{{-- Google Analytics Code --}}
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- backend/views/SchedulePlannerDevelopment.blade.php
+++ backend/views/SchedulePlannerDevelopment.blade.php
@@ -16,6 +16,8 @@
   var main = new org.riceapps.controllers.SchedulePlannerController();
   main.start();
 </script>
+
+{{-- Google Analytics Code --}}
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
